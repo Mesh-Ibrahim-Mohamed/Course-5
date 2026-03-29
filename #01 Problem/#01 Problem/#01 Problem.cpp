00ac8f6edca463681020160db38bef97fc3b3847
ibrahim <ib2007a@gmail.com>
2026-03-29 11:24:44 +0200

    Frist Commitel

---
+++ #01 Problem/#01 Problem/#01 Problem.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+
+	cout << "Multipliction Table From 1 To 10\n\n";
+	cout << "    ";
+
+	for (int i = 1; i <= 10; i++)
+	{
+		cout << i << "\t";
+	}
+	cout << endl;
+	cout << "-----------------------------------------------------------------------\n";
+
+	for (int i = 1; i <= 10; i++)
+	{
+		if (i < 10) 
+			cout << i << "    |  "; 
+		else 
+			cout << i << "   |  ";
+
+		for (int j = 1; j <= 10; j++)
+		{
+			cout << " " << i * j << "\t" ;
+		}
+		cout << endl;
+	}
+}
